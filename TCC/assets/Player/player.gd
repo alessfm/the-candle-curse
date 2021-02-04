1826a2b60399d8bbaf4c0c8fe6c8026ffbe707b1
alessfm <11_aleXS@protonmail.com>
2021-02-04 11:45:42 -0300

    world and player scenes basic
    player moves still simple

---
+++ TCC/assets/Player/player.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+var speed = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		speed = input_vector
+	else:
+		speed = Vector2.ZERO
+		
+	move_and_collide(speed)
