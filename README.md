59212de9447c8434f7e57f5524d106bb88077377
alessfm <11_aleXS@protonmail.com>
2021-03-07 18:49:10 -0300

    create main tile and player

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # The Candle Curse
  My first game
+ Following tutorials...
+ Assets are not mine
--- TCC/assets/Player/player.gd
+++ TCC/assets/Player/player.gd
@@ -6,6 +6,8 @@ const stop_speed = 500
 
 var speed = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -14,8 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		speed = speed.move_toward(input_vector * max_speed, boost_speed * delta)
 	else:
+		animationState.travel("Idle")
 		speed = speed.move_toward(Vector2.ZERO, stop_speed * delta)
 		
 	speed = move_and_slide(speed)
