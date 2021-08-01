08f0a3062dc3d781eae71a5e5e58875446bf1312
alessfm <alessandrofm@aluno.fapce.edu.br>
2021-08-01 11:21:01 -0300

    clean player code

--- TCC/assets/Player/player.gd
+++ TCC/assets/Player/player.gd
@@ -1,18 +1,39 @@
 extends KinematicBody2D
 
-const boost_speed = 500
-const max_speed = 75
-const stop_speed = 500
+# Player State Machine #
+enum{
+	move,
+	roll,
+	attack
+}
 
+var state = move
 var speed = Vector2.ZERO
+# Player State Machine #
+
+
+# Player Animation Layer #
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+#runs at game start
 func _ready():
 	animationTree.active = true
+# Player Animation Layer #
 
+
+# Player Moviment Control #
+const boost_speed = 500
+const max_speed = 75
+const stop_speed = 500
+
+#controls player phisics
 func _physics_process(delta):
+	move_state(delta)
+
+#controls move
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -28,3 +49,5 @@ func _physics_process(delta):
 		speed = speed.move_toward(Vector2.ZERO, stop_speed * delta)
 		
 	speed = move_and_slide(speed)
+# Player Moviment Control #
+
